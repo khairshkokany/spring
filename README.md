7203e23cc1070f5d0daf33f6da9c0168444eba8d
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-10-16 15:46:39 +0300

    finished with testing , read me file

---
+++ README.md
@@ -0,0 +1,11 @@
+#SONGR
+* Create 3 classes with two for songr and one for albums and make a relations between them .
+* create html files to appear the data from post and get methods .
+* create a two repository for album and user and post the data in database using SPRING .
+
+How to run the app
+- Press click of Run in IJ  and open localhost 8080 and add all endPoints to check the contents of the page.
+- you can add album by using  localhost:8080/albums .
+- you can show all the songs by using localhost:8080/songs path .
+- you can add song to specific album localhost:8080/songs. 
+- you can show the specific album  localhost:8080/songs/{album id } for example localhost:8080/songs/1 . 
--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -2,11 +2,31 @@ package com.example.songr;
 
 import com.example.songr.model.Album;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 
 @SpringBootTest
 class SongrApplicationTests {
+
+
+	@Autowired
+	private MockMvc mockMvc;
+
 
 	@Test
 	void contextLoads() {
@@ -41,5 +61,21 @@ class SongrApplicationTests {
 		album.setArtist("bille elish");
 		assertEquals("bille elish",album.getArtist());
 	}
+
+
+//	@Test
+//	public void testReqStatusAndReturnString() throws Exception {
+//
+//		this.mockMvc.perform(get("/hello")).andDo(print())
+//				.andExpect(status().isOk()).
+//				andExpect((ResultMatcher) content().string(containsString("Hello")));
+//	}
+
+	@Test
+	public void testRequestStatusAndReturn() throws Exception{
+		this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello")));
+	}
+
 
 }
